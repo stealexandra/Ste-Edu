cf4b64b4776cf0bd467782ff3e63caa6a7992a51
stealexandra <111023100+stealexandra@users.noreply.github.com>
2022-09-14 19:37:19 -0300

    criando a classe cliente.cs

---
+++ Cliente.cs
@@ -0,0 +1,11 @@
+namespace POOL{
+
+    public class Cliente:Pessoa{
+
+        public Cliente(){}
+        public Cliente(string nome, Endereco endereco, string nascimento, string cpf, string rg):base(nome, endereco, nascimento, cpf, rg){
+        }
+        
+    }
+    
+}
