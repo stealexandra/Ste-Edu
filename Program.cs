3b03044b63965989b0327cc4e17304e55d9cc267
stealexandra <111023100+stealexandra@users.noreply.github.com>
2022-09-14 18:00:01 -0300

    Criando Program.cs
    Criando o programa;
    Introdução ao menu principal;
    Switch case e break;
    Criação de funções

---
+++ Program.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+namespace POO{
+    class Program{
+        static void Main(string[] args){
+            Console.Clear();
+            Console.WriteLine("Olá, seja bem-vindo ao sistema de vendas Edu&Ste");
+            Console.WriteLine("Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            MenuPrincipal();   
+        }
+        public static void MenuPrincipal(){
+            Console.Clear();
+            Console.WriteLine("Bem-vindo ao menu principal. ");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("* Digite 1 para cadastrar funcionário; ");
+            Console.WriteLine("* Digite 2 para cadastrar cliente; ");
+            Console.WriteLine("* Digite 3 para cadastrar produto; ");
+            Console.WriteLine("* Digite 4 para criar venda; ");
+            Console.WriteLine("* Digite 5 para gerenciar funcionários;");
+            int opcao;
+            opcao = int.Parse(Console.ReadLine());
+            switch (opcao)
+            {
+                case 1:
+                    CadFuncionario();
+                break;
+
+                case 2:
+                    CadCliente();
+                break;
+
+                case 3:
+                    CadProduto();
+                break;
+
+                case 4:
+                    CadVenda();
+                break;
+
+                case 5:
+                    GeFunc();
+                break;
+
+                default:
+
+                break;
+            }
+        }
+        
+        public static void CadFuncionario(){
+
+        }
+        public static void CadCliente(){}
+        public static void CadProduto(){}
+        public static void CadVenda(){}
+        public static void GeFunc(){}
+    }
+}
