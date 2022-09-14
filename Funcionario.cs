382e1d4819f28547d2c55194beabfc1423b564f0
EduPaggi <110946818+EduPaggi@users.noreply.github.com>
2022-09-14 18:10:55 -0300

    Criando funcionario.cs

---
+++ Funcionario.cs
@@ -0,0 +1,12 @@
+namespace POOL{
+    public class Func:pessoa{
+        private int salario;
+        public int GetSalario(){
+            return salario;
+        }
+        public void SetSalario(int salario){
+            this.salario = salario;
+        }
+        
+    }
+}
