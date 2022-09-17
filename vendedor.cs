98e6f5e400f97ea0baab9b17655e700fda4d1623
stealexandra <111023100+stealexandra@users.noreply.github.com>
2022-09-16 21:57:56 -0300

    Update vendedor.cs

--- vendedor.cs
+++ vendedor.cs
@@ -1,5 +1,10 @@
-namespace POOL{
+namespace POO{
     public class Vendedor:Func{
         
+        public void dadosVen(){
+            dados();
+            Console.WriteLine("Qual o salário do vendedor?");
+            salario = int.Parse(Console.ReadLine());
+        }
     }
 }
